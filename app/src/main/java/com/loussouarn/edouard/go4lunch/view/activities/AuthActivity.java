3e65801736078355ad77b6f9ae632244c0294f6e
Edouard Loussouarn <ed.loussouarn@gmail.com>
2021-09-25 11:37:08 +0200

    Initial commit

---
+++ app/src/main/java/com/loussouarn/edouard/go4lunch/view/activities/AuthActivity.java
@@ -0,0 +1,14 @@
+package com.loussouarn.edouard.go4lunch.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.loussouarn.edouard.go4lunch.R;
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/loussouarn/edouard/go4lunch/view/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.loussouarn.edouard.go4lunch.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.loussouarn.edouard.go4lunch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
