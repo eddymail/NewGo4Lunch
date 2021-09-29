b672533ccb50d3a4610b145ca33bcc13874fb71f
Edouard Loussouarn <ed.loussouarn@gmail.com>
2021-09-29 09:36:18 +0200

    DrawerLayout fixe

--- app/src/main/java/com/loussouarn/edouard/go4lunch/view/activities/MainActivity.java
+++ app/src/main/java/com/loussouarn/edouard/go4lunch/view/activities/MainActivity.java
@@ -1,16 +1,142 @@
 package com.loussouarn.edouard.go4lunch.view.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.loussouarn.edouard.go4lunch.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+    private NavigationView navigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.mapViewFragment:
+                    //     loadFragment(fragmentMap);
+                    return true;
+                case R.id.listViewFragment:
+                    //   fragment = new ListViewFragment();
+                    // loadFragment(fragment);
+                    return true;
+                case R.id.workmatesListFragment:
+                    //  fragment = new WorkMateFragment();
+                    // loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureToolBar();
+        configureDrawerLayout();
+        configureNavigationView();
+    }
+
+    private void configureNavigationView() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    private void configureDrawerLayout() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureToolBar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_main_frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.detach(fragment);
+        transaction.attach(fragment);
+        transaction.commit();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_drawer_lunch:
+                //     showLunch();
+                break;
+            case R.id.menu_drawer_settings:
+
+                break;
+            case R.id.menu_drawer_logout:
+                //   logOut();
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+  /*  private void showLunch() {
+        if (currentUser.getRestaurantOfTheDay() != null) {
+            Intent intent = new Intent(MainActivity.this, RestaurantDetailsActivity.class);
+            intent.putExtra("goRestaurant", currentUser.getRestaurantOfTheDay());
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.main_activity_no_choose_restaurant), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void logOut() {
+        AuthUI.getInstance().signOut(this).addOnSuccessListener(this, aVoid ->
+        {
+            if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.main_activity_success_log_out), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }*/
+
+
+    @Override
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
